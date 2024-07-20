04f13c0068746bf282da02a32d2aa2f548042ac5
znatii <128041665+znatii@users.noreply.github.com>
2024-07-20 13:29:19 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,44 +8,16 @@ fi
 
 SCRIPT_DIR=$(dirname $(realpath $0))
 
-# Define the alias
-echo "The following allias will be added to your shell for easy tool usage"
-ALIAS="alias toolsave='cd $SCRIPT_DIR && python3 main.py \"$@\" && cd -'"
-echo $ALIAS
+# Define the name for the command
+COMMAND_NAME="toolsave"
 
-# Determine the appropriate shell configuration file
-SHELL_CONFIG=""
-case "$SHELL" in
-    */bash)
-        SHELL_CONFIG="$HOME/.bashrc"
-        ;;
-    */zsh)
-        SHELL_CONFIG="$HOME/.zshrc"
-        ;;
-    */fish)
-        SHELL_CONFIG="$HOME/.config/fish/config.fish"
-        ;;
-    *)
-        echo "Unsupported shell. Please add the following alias manually to your shell configuration file:"
-        echo $ALIAS
-        exit 1
-        ;;
-esac
+# Create a wrapper script in /bin
+WRAPPER_SCRIPT="/bin/$COMMAND_NAME"
+echo "#!/bin/bash" > $WRAPPER_SCRIPT
+echo "cd $SCRIPT_DIR && python3 main.py && cd -" >> $WRAPPER_SCRIPT
 
-# Check if the alias already exists
-if grep -Fxq "$ALIAS" $SHELL_CONFIG; then
-    echo "Alias already exists in $SHELL_CONFIG"
-else
-    echo $ALIAS >> $SHELL_CONFIG
-    echo "Alias added to $SHELL_CONFIG"
-fi
+# Make the wrapper script executable
+chmod +x $WRAPPER_SCRIPT
 
-# Reload the shell configuration
-if [ "$SHELL" = "/usr/bin/fish" ]; then
-    source $SHELL_CONFIG
-else
-    exec $SHELL
-fi
-
-echo "Installation complete. You can now use ToolSave by typing 'toolsave' in your terminal."
+echo "Installation complete. You can now use ToolSave by typing '$COMMAND_NAME' in your terminal."
 
